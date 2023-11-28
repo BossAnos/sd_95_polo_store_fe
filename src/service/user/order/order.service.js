2a4ac693dd09e6ff05007ea09f5f60182eb9a63b
huong03 <phamquanghuong03@gmail.com>
2023-11-28 18:36:09 +0700

    huong03

--- src/service/user/order/order.service.js
+++ src/service/user/order/order.service.js
@@ -2,8 +2,13 @@ import { userAuthService } from "../auth";
 import { userClient } from "../http";
 
 export const getOrderForUser = async () => {
-    const {id} = userAuthService.getAuthInfo();
-    return await userClient.get(`http://localhost:8080/customer/order/${id}`);
-  };
+  const { id } = userAuthService.getAuthInfo();
+  return await userClient.get(`http://localhost:8080/customer/order/${id}`);
+};
 
-  
+export const addOrder = async (id, form) => {
+  return await userClient.post(
+    `http://localhost:8080/customer/order/${id}`,
+    form
+  );
+};
