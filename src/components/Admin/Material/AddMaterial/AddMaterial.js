500c71e7125b07f2a87a218d8c2017c4fefa73e4
zluong24 <zluong24@gmail.com>
2023-12-17 23:39:55 +0700

    update

--- src/components/Admin/Material/AddMaterial/AddMaterial.js
+++ src/components/Admin/Material/AddMaterial/AddMaterial.js
@@ -1,14 +1,32 @@
-import { Button, Form, Input } from "antd";
+import { Button, Form, Input, Modal } from "antd";
 import { materialService } from "../../../../service/admin";
 import { useNavigate, useParams } from "react-router-dom";
 import { toastService } from "../../../../service/common";
-const AddMaterial = () => {
+import XRegExp from "xregexp";
+const AddMaterial = ({ onMateriaFinish, open, onCancel }) => {
   const navigate = useNavigate();
-  const addMateriallHandle = async (form) => {
+  const [materiaForm] = Form.useForm();
+
+  const validateInput = (rule, value, callback) => {
+    const regex = XRegExp("^[\\p{L}0-9\\s]+$");
+    const maxLength = 50;
+
+    if (value && value.length > maxLength) {
+      callback(`Không vượt quá ${maxLength} kí tự`);
+    } else if (value && !regex.test(value)) {
+      callback("Không chứa ký tự đặc biệt");
+    } else {
+      callback();
+    }
+  };  
+
+  const addMaterialHandle = async (form) => {
     try {
-     materialService.createMaterial(form);
-      toastService.success("Thêm chất liệu thành công");
-      navigate("/admin/material");
+      const res = materialService.createMaterial(form);
+      materiaForm.resetFields();
+      toastService.success("Thêm vật liệu thành công");
+      const data = res.data;
+      onMateriaFinish(data);
     } catch (error) {
       console.log(error);
       toastService.error(error.apiMessage);
@@ -16,31 +34,40 @@ const AddMaterial = () => {
   };
 
   return (
-    <Form
-      onFinish={addMateriallHandle}
-      labelCol={{ span: 4 }}
-      wrapperCol={{ span: 8 }}
-    >
-      <Form.Item
-        label="Tên"
-        name="name"
-        rules={[{ required: true, message: "Tên không được trống" }]}
+    <Modal title="Thêm vật liệu" open={open} footer={null} onCancel={onCancel}>
+      <Form
+        onFinish={addMaterialHandle}
+        labelCol={{ span: 4 }}
+        wrapperCol={{ span: 8 }}
+        form={materiaForm}
       >
-        <Input />
-      </Form.Item>
-      <Form.Item
-        label="Tên"
-        name="description"
-        rules={[{ required: true, message: "Tên không được trống" }]}
-      >
-        <Input />
-      </Form.Item>
-      <Form.Item wrapperCol={{ offset: 4 }}>
-        <button type="primary" htmlType="submit">
-          Thêm chất liệu 
-        </button>
-      </Form.Item>
-    </Form>
+        <Form.Item
+          label="Tên"
+          name="name"
+          rules={[
+            { required: true, message: "Tên không được trống" },
+            { validator: validateInput },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="Mô tả"
+          name="description"
+          rules={[
+            { required: true, message: "Mô tả không được trống" },
+            { validator: validateInput },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item wrapperCol={{ offset: 4 }}>
+          <button type="primary" htmlType="submit">
+            Thêm thương hiệu
+          </button>
+        </Form.Item>
+      </Form>
+    </Modal>
   );
 };
 
--- src/components/Admin/Material/Material.js
+++ src/components/Admin/Material/Material.js
@@ -1,24 +1,25 @@
 import { useEffect, useState } from "react";
 import { materialService } from "../../../service/admin";
-import { Button, Popconfirm, Tabs } from "antd";
+import { Button, Popconfirm, Tabs, Form, Input, Switch } from "antd";
 import { Link } from "react-router-dom";
-import "../admin-product.css"
+import { AddMaterial } from "./AddMaterial/AddMaterial";
+import { useNavigate, useParams } from "react-router-dom";
 import { toastService } from "../../../service/common";
+import "../admin-product.css";
 
 const { TabPane } = Tabs;
-const handleDelete = async (id) => {
-  const body = await materialService.changeStatus(id);
 
-  toastService.info("Thay đổi trạng thái thành công ");
-};
 const tabs = [
-
+  {
+    key: "all",
+    label: "Tất cả",
+    status: null,
+  },
   {
     key: "1",
     label: "Đang hoạt động",
     status: 1,
   },
-
   {
     key: "0",
     label: "Ngừng hoạt động",
@@ -27,24 +28,70 @@ const tabs = [
 ];
 
 const MaterialList = () => {
+  const [searchProductName, setSearchProductName] = useState("");
   const [material, setMaterial] = useState([]);
-  const [activeTab, setActiveTab] = useState("1");
+  const [activeTab, setActiveTab] = useState("all");
+  const [showMaterialModal, setShowMaterialModal] = useState(false);
+  const [form] = Form.useForm();
 
   useEffect(() => {
-    (async () => {
+    const fetchData = async () => {
       const body = await materialService.getAllMaterial();
-      console.log("All Material:", body.data); // Add this line to check the data received
+      console.log("All Material:", body.data);
       setMaterial(body.data);
-    })();
+    };
+
+    fetchData();
   }, []);
 
   const handleTabChange = (key) => {
     setActiveTab(key);
   };
 
-  const filteredmaterials = material.filter(
-    (item) => item.status === parseInt(activeTab, 10)
-  );
+  const filteredMaterials = material.filter((item) => {
+    const statusMatch =
+      activeTab === "all" || item.status === parseInt(activeTab, 10);
+
+    const nameMatch =
+      item.name &&
+      item.name.toLowerCase().includes(searchProductName?.toLowerCase() || "");
+
+    const descriptionMatch =
+      item.description &&
+      item.description
+        .toLowerCase()
+        .includes(searchProductName?.toLowerCase() || "");
+
+    const indexMatch = (item.index + 1)
+      .toString()
+      .includes(searchProductName?.toString() || "");
+
+    return statusMatch && (nameMatch || descriptionMatch || indexMatch);
+  });
+
+  const handleDelete = async (id) => {
+    const body = await materialService.changeStatus(id);
+
+    toastService.info("Thay đổi trạng thái thành công ");
+  };
+
+  async function toggleStatus(id, currentStatus) {
+    const newStatus = currentStatus === 1 ? 0 : 1;
+    await materialService.changeStatus(id, newStatus);
+
+    // Update the local state with the new status
+    setMaterial((prevMaterials) =>
+      prevMaterials.map((material) =>
+        material.id === id ? { ...material, status: newStatus } : material
+      )
+    );
+  }
+
+  async function createMaterial(newMaterial) {
+    const createdMaterial = await materialService.createMaterial(newMaterial);
+    setMaterial((prevMaterials) => [...prevMaterials, createdMaterial]);
+    setShowMaterialModal(false);
+  }
 
   return (
     <div
@@ -54,39 +101,67 @@ const MaterialList = () => {
         boxShadow: "0 0 10px rgba(0, 0, 0, 0.5)",
       }}
     >
+      <AddMaterial
+        open={showMaterialModal}
+        onMaterialFinish={createMaterial}
+        onCancel={() => setShowMaterialModal(false)}
+      />
       <Tabs activeKey={activeTab} onChange={handleTabChange}>
         {tabs.map((tab) => (
           <TabPane tab={tab.label} key={tab.key} />
         ))}
       </Tabs>
       <br />
-      <Link to={"/admin/material/add"}>
-        <Button type="primary" className="btn-customer__add ">Thêm vật liệu</Button>
-      </Link>
+      <div style={{ display: "flex", marginLeft: "0px" }}>
+        <button
+          onClick={() => setShowMaterialModal(true)}
+          type="primary"
+          className="btn-customer__add "
+        >
+          Thêm vật liệu
+        </button>
+        <p style={{ fontWeight: "bolder", fontSize: "20px" }}>Tìm kiếm:</p>
+        <Input
+          style={{ width: "300px", marginLeft: "30px" }}
+          placeholder="Search by product name..."
+          value={searchProductName}
+          onChange={(e) => setSearchProductName(e.target.value)}
+        />
+      </div>
       <br />
       <br />
       <div className="table__main">
-      <table>
-        <thead>
-          <tr>
-            <th>STT</th>
-            <th>Tên màu sắc</th>
-            <th>Mô tả</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {filteredmaterials.length === 0 ? (
+        <table>
+          <thead>
             <tr>
-              <td colSpan="4">Không có giá trị.</td>
+              <th>STT</th>
+              <th>Tên vật liệu</th>
+              <th>Mô tả</th>
+              <th>Trạng thái</th>
+              <th>Actions</th>
             </tr>
-          ) : (
-            filteredmaterials.map((material, index) => {
-              return (
+          </thead>
+          <tbody>
+            {filteredMaterials.length === 0 ? (
+              <tr>
+                <td colSpan="5">Không có giá trị.</td>
+              </tr>
+            ) : (
+              filteredMaterials.map((material, index) => (
                 <tr key={material.id}>
-                  <td style={{paddingLeft:"60px"}}>{index + 1}</td>
+                  <td style={{ paddingLeft: "60px" }}>{index + 1}</td>
                   <td>{material.name}</td>
                   <td>{material.description}</td>
+                  <td>
+                    <Switch
+                      checked={material.status === 1}
+                      onChange={() => toggleStatus(material.id, material.status)}
+                      style={{
+                        backgroundColor: material.status === 1 ? "green" : "red",
+                        width: "30px",
+                      }}
+                    />
+                  </td>
                   <td>
                     <div
                       className="actions"
@@ -99,30 +174,17 @@ const MaterialList = () => {
                           </Button>
                         </Link>
                       </div>
-                      <div className="action">
-                      <Popconfirm
-                            title="Đổi trạng thái"
-                            description="Bạn có chắc chắn muốn thay đổi trạng thái?"
-                            onConfirm={() => handleDelete(material.id)}
-                            okText="Yes"
-                            cancelText="No"
-                          >
-                            <button className="btn">
-                              <i className="fa-sharp fa-solid fa-trash"></i>
-                            </button>
-                          </Popconfirm>
-                      </div>
                     </div>
                   </td>
                 </tr>
-              );
-            })
-          )}
-        </tbody>
-      </table>
+              ))
+            )}
+          </tbody>
+        </table>
       </div>
     </div>
   );
-};                                                   
+};
 
-export {MaterialList };
+export { MaterialList };
+ 
